7a75fa252efe416c7405157ea95ff27fd1e04de9
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-03-03 22:40:10 +0300

    Project Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace numberGet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

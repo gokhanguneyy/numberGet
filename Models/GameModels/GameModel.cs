f7d8e36e4b7be75deccae0eeeec1349604671aae
gokhanguneyy <101227317+gokhanguneyy@users.noreply.github.com>
2025-03-21 14:33:24 +0300

    Added GameModel

---
+++ Models/GameModels/GameModel.cs
@@ -0,0 +1,13 @@
+﻿using numberGet.Enums;
+
+namespace numberGet.Models.GameModels
+{
+    public class GameModel
+    {
+        public int? UserGuess { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public int RemainingAttempts { get; set; }
+        public GameLevelsEnum SelectedLevel { get; set; }
+        public bool IsGameOver { get; set; } = false;
+    }
+}
